587984ef477e63f79a2340dd26e800f6989ee883
nicecoffeday <71484908+nicecoffeday@users.noreply.github.com>
2021-09-30 00:12:32 +0800

    Add files via upload

--- game.py
+++ game.py
@@ -1,27 +1,63 @@
+# 操控 sprite
 import pygame
-
+W = 500
+H = 600
 FPS = 60
 white = (255, 255, 255)
+GREEN = (0 , 255, 0)
 
 
-# game initialization
+# game initializ ation
 pygame.init()
-clock pyhon.time.Clock
-screan = pygame.display.set_mode((500, 600)) #set screan size
+screan = pygame.display.set_mode((W, H)) #set screan size
+pygame.display.set_caption('First game')
+clock = pygame.time.Clock()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = (W/2)
+        self.rect.bottom = H - 10
+        self.speedx = 8
+
+    def update(self):
+        key_pressed = pygame.key.get_pressed()# 回傳布林直 控制鍵盤
+        if key_pressed[pygame.K_RIGHT]: # 判斷右鍵有沒有觸發
+            self.rect.x += self.speedx
+        if key_pressed[pygame.K_LEFT]: # 判斷左鍵鍵有沒有觸發
+            self.rect.x -= self.speedx
+        if self.rect.right > W:
+            self.rect.right = W
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+
+
 
 running = True
 
 while running:
-	clock.tick(FPS) # limit loop times FPS
+    clock.tick(FPS) # limit loop times FPS
 
-	#Get input
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
+    #Get input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-	#Update
+    #Update
+    all_sprites.update()
 
-	#display
-	screan.fill((white)) # Adjust color use rgb
-	python.display.update()
+ 
+    #display
+    screan.fill((white)) # Adjust color use rgb
+    all_sprites.draw(screan)
+    pygame.display.update()
 pygame.quit()
